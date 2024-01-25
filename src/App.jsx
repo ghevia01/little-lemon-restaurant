8a45a5e0834305515ca4a0e1949dbe40f91296f9
Gean Hevia <gcaarlos30@gmail.com>
2024-01-24 22:03:00 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React App</h1>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
