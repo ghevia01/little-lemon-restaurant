8de3775c36935708eb304202e9990a4a4739432e
Gean Hevia <ghevia.workspace@gmail.com>
2024-01-28 11:24:20 -0600

    feat(header): add logo, navigation, and buttons

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,9 +1,19 @@
 // import React from "react";
+import Logo from "../../common/Logo/Logo";
+import HeaderNav from "../Nav/HeaderNav";
+import Button from "../../common/Button/Button"
+
+import "./header.scss";
 
 function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="header flex justify-between align-center">
+      <Logo />
+      <HeaderNav />
+      <div className="button-group flex justify-center align-center">
+        <Button variant="none">Login</Button>
+        <Button variant="primary">Sign up</Button>
+      </div>
     </header>
   );
 }
