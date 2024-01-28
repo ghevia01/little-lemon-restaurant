38e6cca1085a5dc5ab7d52e2700a993d5a84a501
Gean Hevia <ghevia.workspace@gmail.com>
2024-01-28 11:43:13 -0600

    feat(routing): add Booking page route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
+import Booking from "./pages/Booking/Booking";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
       </Routes>
     </Router>
   );
