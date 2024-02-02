06af640a092be0bfa812273b8f8ee6b4c7d51832
Gean Hevia <ghevia.workspace@gmail.com>
2024-02-02 13:11:18 -0600

    feat(routing): add ConfirmedBooking route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Booking from "./pages/Booking/Booking";
+import ConfirmedBooking from "./pages/ConfirmedBooking/ConfirmedBooking"
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/confirmed-booking" element={<ConfirmedBooking />} />
       </Routes>
     </Router>
   );
