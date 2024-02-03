b1cd8faf11ffc2a2bbaad815ac49bf3bf297c8f0
Gean Hevia <ghevia.workspace@gmail.com>
2024-02-03 11:15:45 -0600

    refactor(component): update Testimonials section, styles and add testimonials,js

--- src/components/sections/Testimonials/Testimonials.jsx
+++ src/components/sections/Testimonials/Testimonials.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import TestimonialCard from "../../common/TestimonialCard/TestimonialCard";
+import { TESTIMONIALS } from "../../../constants/testimonials";
+
+import "./testimonials.scss";
 
 function Testimonials() {
   return (
-    <footer>
-      <h1>Testimonials</h1>
-    </footer>
+    <section className="testimonials">
+      <div className="testimonials-header">
+        <h2>Tales from the Table</h2>
+        <p>What our customers are saying about us</p>
+      </div>
+      <div className="specials-grid">
+        {TESTIMONIALS.map((testimonial) => (
+          <TestimonialCard key={testimonial.name} {...testimonial} />
+        ))}
+      </div>
+    </section>
   );
 }
 
